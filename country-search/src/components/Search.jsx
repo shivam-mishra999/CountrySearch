c9f6961ebcf53c78c56abe0a8386022f765d777c
Shivam <mishra.shiva999@gmail.com>
2024-03-01 15:37:35 +0530

    first

---
+++ country-search/src/components/Search.jsx
@@ -0,0 +1,41 @@
+import React, {useState, useEffect} from 'react';
+import styles from "./Search.module.css";
+
+export default function Search() {
+    const [country, setCountry] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+
+    useEffect(()=>{
+        fetch("https://restcountries.com/v3.1/all")
+        .then((response)=> response.json())
+        .then((data)=>setCountry(data))
+        .catch((error)=>{
+            console.log("Error in fetching countries: ", error);
+        })
+    },[])
+
+    const handleChange = (event) => {
+        setSearchTerm(event.target.value);
+    }
+
+    const filteredCountries = country.filter((country) => (
+        country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
+    ));
+  return (
+    <div className={styles.container}>
+        <div className={styles.input}>
+            <input type="text" placeholder="search for countries" value={searchTerm} onChange={handleChange} />
+        </div>
+        
+        <div className={styles.countryContainer}>
+            {filteredCountries.map((country) => (
+                <div className={styles.countryCard} key={country.cca3}>
+                    <img className={styles.flag} src={country.flags.png} alt={country.cca3}/>
+                    <h2>{country.name.common}</h2>
+                </div>
+            ))}
+        </div>
+      
+    </div>
+  )
+}
